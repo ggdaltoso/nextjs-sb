f0e527caec71d6b68073c5ec5b88c59c113eac1d
Matheus Martins <mathews_2010@outlook.com>
2023-04-14 15:00:59 -0300

    web-publisher: Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>About me &rarr;</h3>
-            <p>Find in-depth information about me and myself.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
